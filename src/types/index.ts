216081b2d1007615a3a054bc8a16f0e44c9afeaf
Johndoe <56298669+togane@users.noreply.github.com>
2022-03-06 09:50:29 +0900

    add MoveDirection

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,15 @@ export type File = typeof ALL_FILES[number];
 
 export const ALL_RANKS = [1, 2, 3, 4, 5, 6, 7, 8] as const;
 export type Rank = typeof ALL_RANKS[number];
+
+export const ALL_MOVE_DIRECTIONS = [
+  'Up',
+  'UpRight',
+  'Right',
+  'DownRight',
+  'Down',
+  'DownLeft',
+  'Left',
+  'UpLeft',
+] as const;
+export type MoveDirection = typeof ALL_MOVE_DIRECTIONS[number];
