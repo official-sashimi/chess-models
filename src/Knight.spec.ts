bac41b51038bce6e53e58de2f5ffadb5f5352dd5
Johndoe <56298669+togane@users.noreply.github.com>
2022-03-06 12:44:19 +0900

    implement Knight

---
+++ src/Knight.spec.ts
@@ -0,0 +1,38 @@
+import { Knight } from './Knight';
+import { Position } from './Position';
+
+describe('Knight', () => {
+  describe('movablePositionsFrom method', () => {
+    const whiteKnight = new Knight('White');
+    const blackKnight = new Knight('Black');
+
+    it('should returns an array of positions', () => {
+      expect(whiteKnight.movablePositionsFrom(new Position('b', 1))).toEqual(
+        new Set([
+          new Position('a', 3),
+          new Position('c', 3),
+          new Position('d', 2),
+        ]),
+      );
+      expect(whiteKnight.movablePositionsFrom(new Position('f', 3))).toEqual(
+        new Set([
+          new Position('d', 2),
+          new Position('d', 4),
+          new Position('e', 1),
+          new Position('e', 5),
+          new Position('g', 1),
+          new Position('g', 5),
+          new Position('h', 2),
+          new Position('h', 4),
+        ]),
+      );
+      expect(blackKnight.movablePositionsFrom(new Position('g', 8))).toEqual(
+        new Set([
+          new Position('e', 7),
+          new Position('f', 6),
+          new Position('h', 6),
+        ]),
+      );
+    });
+  });
+});
