0c88953f94caa7e8cca077f1226b932515849bd4
Johndoe <56298669+togane@users.noreply.github.com>
2022-03-09 06:18:29 +0900

    add an entry point

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { Position } from './Position';
+
+// piece models
+export { Bishop } from './Bishop';
+export { King } from './King';
+export { Knight } from './Knight';
+export { Pawn } from './Pawn';
+export { Queen } from './Queen';
+export { Rook } from './Rook';
